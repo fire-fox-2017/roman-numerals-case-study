590980a599837fa859972e79291c6bb16b036452
akbarsahata <akbarsahata@outlook.co.id>
2017-03-27 11:13:57 +0700

    Template

---
+++ roman_numerals.js
@@ -0,0 +1,23 @@
+/*
+Please answer the questions below :
+
+1. Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
+
+2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
+
+3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
+
+4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
+
+5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
+
+6. Adakah struktur data baru? Apa saja, tolong sebutkan
+
+7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
+
+8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
+
+9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
+
+10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
+*/
