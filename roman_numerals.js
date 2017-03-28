b0ad993fd161623aa5423b9998d0addcb4d965a0
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-03-28 21:45:23 +0800

    done

--- roman_numerals.js
+++ roman_numerals.js
@@ -2,22 +2,40 @@
 Please answer the questions below :
 
 1. Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
+bisa di run pake node.js
 
 2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
+coba dijalankan dengan kasih driver code beberapa test cases.
+di solusi 1 ada bugs: utk angka mulai dari 4000 ke atas tidak di convert ke roman numerals
+solusi 3: forever loop if num > 1000
+
 
 3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
+unary operator di depan variable
+
 
 4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
+solusi 2 yg sudah dimengerti, solusi lainnya bisa dimengerti
 
 5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
+bisa
 
 6. Adakah struktur data baru? Apa saja, tolong sebutkan
+tidak ada
+hanya memakai array utk store roman numeral dan translasi numbers index
 
 7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
 
 8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
+solution 4
+pendek, effisien, dan mudah di mengerti
+
 
 9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
+karena mudah dimengerti, kita bisa tahu performa applikasinya (hanya ada 2 loop: outer loop dan inner loop).
 
 10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
+kalau pattern code nya pendek, efisien dan mudah dimengerti, sangat mudah utk melakukan code maintenance.
+
+
 */
