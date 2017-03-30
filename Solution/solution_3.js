fafd9fc2f868b7a4095d45f6a74542fe2cc1da80
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-03-30 20:40:57 +0700

    Komit1

---
+++ Solution/solution_3.js
@@ -0,0 +1,27 @@
+var to_roman = function(num){
+  var decimalValue = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  var romanNumeral = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+
+  var numCopy = num;
+  var romanized = '';
+
+  while (numCopy > 0){
+    for (var index = 0; index < decimalValue.length; index++){
+      if (+decimalValue[index] <= numCopy && +decimalValue[+index + 1] > numCopy){
+        romanized += romanNumeral[index];
+        numCopy -= decimalValue[index];
+      }
+    }
+  }
+
+  return romanized;
+};
+
+console.log('My totally sweet testing script for new roman\n')
+console.log('input | expected | actual')
+console.log('———|—————|———')
+console.log('4     | IV       | ', to_roman(4))
+console.log('9     | IX       | ', to_roman(9))
+console.log('13    | XIII     | ', to_roman(13))
+console.log('1453  | MCDLIII  | ', to_roman(1453))
+console.log('1646  | MDCXLVI  | ', to_roman(1646))
---
+++ Solution/solution_4.js
@@ -0,0 +1,21 @@
+function to_roman(num){
+  var result = '';
+  var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ["M", "MC","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+  for (var i = 0; i <= decimal.length; i++){
+    while (num%decimal[i] < num){
+      result += roman[i];
+      num -= decimal[i];
+    }
+  }
+  return result;
+}
+
+console.log('My totally sweet testing script for new roman\n')
+console.log('input | expected | actual')
+console.log('———|—————|———')
+console.log('4     | IV       | ', to_roman(4))
+console.log('9     | IX       | ', to_roman(9))
+console.log('13    | XIII     | ', to_roman(13))
+console.log('1453  | MCDLIII  | ', to_roman(1453))
+console.log('1646  | MDCXLVI  | ', to_roman(1646))
