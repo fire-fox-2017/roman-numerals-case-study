16a084bb5ccc9da3ef4d2ec7f372a721fcf382ac
Parel Wellman Hutahaean <parel.hutahaean@gmail.com>
2017-03-31 02:01:50 +1400

    Roman Numerals Case Study Finished

--- roman_numerals.js
+++ roman_numerals.js
@@ -2,22 +2,32 @@
 Please answer the questions below :
 
 1. Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
+Jawaban : Semua solusi bisa dieksekusi. Cara memverifikasinya dengan command node [FILENAME], apabila tidak keluar pesan error di command line maka berhasil dieksekusi.
 
 2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
+Jawaban : Solusi 3 mengandung bugs yaitu infinite looping pada saat param input 1000 ke atas. Setelah dijalankan program tidak closed.
 
 3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
+Jawaban : Saya baru mengetahui ada operator unary plus (+).
 
 4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
+Jawaban : Solusi 2 dan solusi 3 masih sulit dimengerti terutama baris kode di dalam while-looping solusi 2 dan baris kode yang mengandung operator unary plus (+).
 
 5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
+Jawaban : Belum. Masih berkutat dengan tugas yang belum selesai sehingga belum ada waktu untuk mempelajari lebih dalam.
 
 6. Adakah struktur data baru? Apa saja, tolong sebutkan
+Jawaban : Tidak ada.
 
 7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
+Jawaban : Solusi 2 dan 3 sangat berbeda dari kemungkinan solusi yang terpikirkan oleh saya. Meskipun solusi 2 berhasil dijalankan tanpa bugs, masih sulit dipahami.
 
 8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
+Jawaban : Solusi 1 adalah solusi terbaik karena selain mudah dipahami, juga bisa menangani test case 4000 ke atas dan 0 ke bawah, sedangkan solusi 4 tidak.
 
 9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
+Jawaban : Mengukur performa dapat dilihat dari banyak komputasi yang dilakukan utk mengeksekusi program. Terlalu banyak loop akan membuat semakin jelek performanya.
 
 10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
+Jawaban : Pola yang ringkas dan mudah dipahami akan memudahkan maintenance code.
 */
