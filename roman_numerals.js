b2b97f59ade76cd32665bec5bbbc308fb15f44bd
aldyadk <aldy.andika@gmail.com>
2017-03-30 17:21:15 +0700

    komit roman numerals case study

--- roman_numerals.js
+++ roman_numerals.js
@@ -2,22 +2,32 @@
 Please answer the questions below :
 
 1. Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
+Belum tentu, karena code perlu dicek apakah mengandung bug terlebih dahulu
 
 2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
+Dengan melakukan debugging
 
 3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
+operator + (unary operator) untuk mengubah string menjadi number
 
 4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
+ya
 
 5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
+kemungkinan besar iya
 
 6. Adakah struktur data baru? Apa saja, tolong sebutkan
+array
 
 7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
+Menurut saya yg berbeda adalah parameter dalam melakukan looping, yg nantinya juga berpengaruh pada performa aplikasi
 
 8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
+Saya akan memilh solusi nomer 2, yakni melakukan looping dengan parameter i yang turun dari 3 menuju 0, karena seharusnya aplikasi bisa melakukan kerja lebih cepat dengan sedikit loop
 
 9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
+Aplikasi akan bekerja lebih cepat, karena makin sedikit melakukan loop, terlihat dari banyak langkah yang ditunjukkan bila dilakukan debug
 
 10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
+Semakin sedikit logic/looping akan lebih mudah dimaintain
 */
