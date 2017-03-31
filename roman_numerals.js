03a30f5406308b5fac36ea6019cf4b80b573709f
stedyyulius <stedyyulius@gmail.com>
2017-03-31 17:23:27 +0700

    second commit

--- roman_numerals.js
+++ roman_numerals.js
@@ -5,19 +5,19 @@ Please answer the questions below :
   bisa, caranya adalah menggunakan command "node" di node js
 
 2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
-  selama proses mencari solusi yang paling tepat jelas banyak sekali bug yang muncul, dan itu semua diketahui melalui command "node" di node js yang memberitahu dengan tepat dimanakah bug itu berasal
+ya tes case 3 infinite loop, cara mengetehauinya adalah node js saya hang coy
 3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
 ada,saya baru tahu modulus dan bagi itu sangat berbeda, dan dengan menggunakan moduluslah case ini bisa saya selesaikan
 4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
-ya tentu saja, saya kan yang bikin
+ya tentu saja
 5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
 tentu saja, sintaks untuk mencarin solusi setiap kasus kebanyakan pasti bersifat luas sehingga bisa kita berlakukan di banyak kasus yang berbeda
 6. Adakah struktur data baru? Apa saja, tolong sebutkan
 ada, dengan membuat 2 baris array yang berbeda dan mencocokkannya satu per satu
 7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
-belum, karena saya hanya mencoba satu macam solusi yaitu dengan mencocokkan angka dengan huruf menggunakan modulus
+gak ah mirip2 smua
 8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
-kembali lagi ke nomor 7,itu adalah solusi terbaik so far
+solusi 4 yang menggunakan modulus karena menurut saya itu yang paling cepat
 9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
 dengan menggunakan metode modulus, sama saja menggunakan metode binary chop yang akan membuat dampak positif terhadap performa aplikasi dikarenakan mempersempit lingkup pencariannya dengan cukup singkat
 10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
