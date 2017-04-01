4de4a57e575b5324b5c39a2d6ae0eaf4c4f643ab
ambo-dalle <ambodalle.st@gmail.com>
2017-04-01 13:56:48 +0700

    Setor tugas Roman case study

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,23 +1,38 @@
 /*
+
+
 Please answer the questions below :
 
 1. Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
+Jawaban : Semua solusi bisa dieksekusi. Memverifikasinya dengan cara menjalankan di jsbin / di command promt dengan command node [FILENAME] , apabila tidak keluar pesan error di command line maka berhasil dieksekusi.
 
 2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
+Jawaban : Ada. Setelah dijalankan program tidak closed timbul pesan error.
 
 3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
+Jawaban : Sintax baru result += roman[i];  num -= decimal[i];.
 
 4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
+Jawaban : Iya semua dapat dimengerti.
 
 5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
+Jawaban : Belum. Belum ada waktu untuk mempelajari lebih dalam karena masih menyelesaikan tugas-tugas.
 
 6. Adakah struktur data baru? Apa saja, tolong sebutkan
+Jawaban : Tidak ada.
 
 7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
+Jawaban : ya, dari segi algoritmanya berbeda walau hasil output hampir sama, ini dikarenakan adanya perbedaan pola pikir kita dalam menuangkan algoritma ke dalam sintax. berbeda dengan Pseudocode yang bersifat universal jika sintax digunakan untuk pemahaman algoritma
+dinilai oleh saya cukup sulit.
+
 
 8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
+Jawaban : Solusi 1 adalah solusi terbaik karena mudah dipahami juga.
 
 9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
+Jawaban : Mengukur performa dapat dilihat dari banyak komputasi yang dilakukan utk mengeksekusi program. ini terkait dengan efisiensi memory semakin ringkas semakin rendah dalam memmakan source memori.
 
 10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
+Jawaban : semakin rendah pemola-an dan pattern pada code akan semakin meringankan kinerja processor, Pola yang ringkas dan mudah dipahami akan memudahkan maintenance code.
+
 */
