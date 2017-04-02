b349a9b9964f3254274cbdf94af0d8586f1bf63d
stedyyulius <stedyyulius@gmail.com>
2017-04-02 21:53:41 +0700

    revisi

--- roman_numerals.js
+++ roman_numerals.js
@@ -9,15 +9,15 @@ ya tes case 3 infinite loop, cara mengetehauinya adalah node js saya hang coy
 3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
 ada,saya baru tahu modulus dan bagi itu sangat berbeda, dan dengan menggunakan moduluslah case ini bisa saya selesaikan
 4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
-ya tentu saja
+nomor 2 aga
 5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
-tentu saja, sintaks untuk mencarin solusi setiap kasus kebanyakan pasti bersifat luas sehingga bisa kita berlakukan di banyak kasus yang berbeda
+tentu saja, yang metode while kayanya lebih simple
 6. Adakah struktur data baru? Apa saja, tolong sebutkan
-ada, dengan membuat 2 baris array yang berbeda dan mencocokkannya satu per satu
+yang pake + itu contoh: +decimalvalue di test case 3
 7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
-gak ah mirip2 smua
+sebenernya mirip2 sih cuma tiap2 orang kan ngeliat yang simplenya menurut mereka berbeda beda
 8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
-solusi 4 yang menggunakan modulus karena menurut saya itu yang paling cepat
+solusi 1, yang pake while diurutin doang
 9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
 dengan menggunakan metode modulus, sama saja menggunakan metode binary chop yang akan membuat dampak positif terhadap performa aplikasi dikarenakan mempersempit lingkup pencariannya dengan cukup singkat
 10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
