341e2f3d5208a887d4ce0d433bd53e10e8fe6f31
aldyadk <aldy.andika@gmail.com>
2017-04-04 10:28:24 +0700

    perbaiki file

--- roman_numerals.js
+++ roman_numerals.js
@@ -2,10 +2,12 @@
 Please answer the questions below :
 
 1. Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
-Tidak, pada solusi 2 & 3 terdapat bugs
+Bisa, namun tidak semua menghasilkan output yg diharapkan seperti pada driver code.
+Caranya adalah menjalankannya dengan node js
 
 2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
-Dengan melakukan debugging
+Ada yg menghasilkan output tidak sesuai dengan yg diharapkan seperti pada driver code.
+Dengan menjalankan program dan melihat hasilnya,atau bsa dengan debugging
 
 3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
 operator + (unary operator) untuk mengubah string menjadi number
@@ -23,7 +25,7 @@ saat ini tidak ada
 Menurut saya yg berbeda adalah parameter dalam melakukan looping, yg nantinya juga berpengaruh pada performa aplikasi
 
 8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
-Saya akan memilh solusi nomer 2 (dengan catatan diperbaiki dahulu bugsnya), yakni melakukan looping dengan parameter i yang turun dari 3 menuju 0, karena seharusnya aplikasi bisa melakukan kerja lebih cepat dengan sedikit loop
+Saya akan memilh solusi nomer 2, yakni melakukan looping dengan parameter i yang turun dari 3 menuju 0, karena seharusnya aplikasi bisa melakukan kerja lebih cepat dengan sedikit loop
 
 9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
 Aplikasi akan bekerja lebih cepat, karena makin sedikit melakukan loop, terlihat dari banyak langkah yang ditunjukkan bila dilakukan debug
